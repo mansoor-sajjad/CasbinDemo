81ae5e2705f5a9eb523f6f8d6ed0b6b08094bbfd
mansoor-sajjad <mansoor.sajjad@hotmail.com>
2026-01-25 09:04:55 +0100

    first commit

---
+++ src/main/java/com/eldmatix/CasbinDemo/CasbinDemoApplication.java
@@ -0,0 +1,13 @@
+package com.eldmatix.CasbinDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasbinDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasbinDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eldmatix/CasbinDemo/CasbinDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eldmatix.CasbinDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasbinDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
